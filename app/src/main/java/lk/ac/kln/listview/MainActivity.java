a389773f3c991212bf4286e023497a5902819106
ishan6 <ishanmenaka6@gmail.com>
2019-06-17 10:12:02 +0530

    Initial2

--- app/src/main/java/lk/ac/kln/listview/MainActivity.java
+++ app/src/main/java/lk/ac/kln/listview/MainActivity.java
@@ -2,6 +2,7 @@ package lk.ac.kln.listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.RelativeLayout;
 
@@ -15,6 +16,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView1);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+
+        adapter = new PizzaAdapter();
+        recyclerView.setAdapter(adapter);
 
 
 
